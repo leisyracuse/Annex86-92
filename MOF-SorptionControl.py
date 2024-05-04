fa6b479a2bb6b86cf3d342b4116102f066c25f92
Jialei Shen <jshen20@syr.edu>
2024-05-04 02:54:19 -0400

    update material sorption model & run a simple case

---
+++ MOF-SorptionControl.py
@@ -0,0 +1,246 @@
+"""
+MOF-SorptionControl.py - simulate air contaminant adsorbed by MOF material.
+"""
+
+import os
+from argparse import ArgumentParser
+from dataclasses import dataclass
+from contamxpy import cxLib
+from material_adsorption import SorptionMaterial
+from material_adsorption import Sorption
+
+
+@dataclass
+class ControlIdxName:
+    """
+    Contains Index and Name of a control within either `cxLib.outputControls`
+    list or `cxLib.inputControls` list.
+    """
+    index: int
+    name: str
+
+# =========================================================================
+# USER INPUT - Set control-related GLOBAL variables.
+# =========================================================================
+#   INDEXes and Names can be obtained by running this script and viewing
+# OUTPUT CONTROLS and INPUT CONTROLS lists. These indices are not
+# necessarily the same as the control IDs in ContamW. They are indices into
+# the corresponding named SET and SPLIT controls in the project file.
+
+T_ROOM = ControlIdxName(1, "T_room")  # PRJ Control to get room temperature, degC (not used)
+HCHO_ROOM = ControlIdxName(2, "HCHO_room")  # PRJ Control to get room HCHO conc, ug/m3
+SINK_SET = ControlIdxName(1, "sink_set")  # PRJ Control to set HCHO sink rate, ug/s
+
+PV_PASS_CTRL = HCHO_ROOM    # process value (pv) passed to controller
+CO_SET_CTRL = SINK_SET      # control output (co) set by the controller
+
+Am = 3      # Material area, m2
+Km = 1e-5   # Average room mass transfer coefficient
+a = -10.908
+b = 9333.1
+material = SorptionMaterial(Am, Km, a, b) # define sorption material
+
+# ====================================================== check_controls() =====
+
+
+def check_controls(cxl, test, pv_ctrl, co_ctrl) -> int:
+    '''
+    Check to see if controls are properly defined within the PRJ.
+    There should be a named SPLIT/PASS control to provide the HCHO conc
+    to this module, i.e., process value (pv) and a named SET control
+    for the sorption sink, i.e., controller output (co).
+
+        Args:
+            cxl: `cxLib` instance for specific PRJ
+            test: `bool`
+                Print list of input/output controls in the PRJ
+            pv_ctrl: `CtrlIdxName`
+                Index and Name of cxLib.outputControls[] list item
+            co_ctrl: `CtrlIdxName`
+                Index and Name of cxLib.inputControls[] list item
+
+        :returns: Number of errors found. 0 if `test` is True
+        :rtype: int
+    '''
+    input_error = 0
+    if test is True:
+        # ----- CONTROLS PROPERTIES
+        print("----- OUTPUT CONTROLS -----")
+        for i, ctrl in zip(range(cxl.nOutputControls), cxl.outputControls):
+            ctrl_name = ctrl.name
+            print(f"{i+1} {ctrl_name}")
+        print("\n")
+        print("----- INPUT CONTROLS -----")
+        for i, ctrl in zip(range(cxl.nInputControls), cxl.inputControls):
+            ctrl_name = ctrl.name
+            print(f"{i+1} {ctrl_name}")
+        print("\n")
+        return input_error
+
+    # Check to see if control indices are set correctly.
+    if pv_ctrl.index > cxl.nOutputControls or \
+       cxl.outputControls[pv_ctrl.index-1].name != pv_ctrl.name:
+        print(f"Incorrect index ({pv_ctrl.index}) for output control: {pv_ctrl.name}")
+        input_error += 1
+    if co_ctrl.index > cxl.nInputControls or \
+       cxl.inputControls[co_ctrl.index-1].name != co_ctrl.name:
+        print(f"Incorrect index ({co_ctrl.index}) for input control: {co_ctrl.name}")
+        input_error += 1
+
+    if input_error > 0:
+        print("Run this script with -t option to determine correct control indices.")
+
+    return input_error
+
+
+# ========================================= callback_set_initial_values() =====
+
+
+def callback_set_initial_values(cxl: cxLib):
+    '''
+    Callback function to initialize sorption sink control value (cv)
+    '''
+    cxl.setInputControlValue(SINK_SET.index, 0) # assuming as 0
+
+
+# ================================================================ main() =====
+
+
+def main():
+    '''
+    This program takes the full name of a PRJ file and runs the simulation from
+    beginning to end. It implements a sorption sink.
+    '''
+    # ----- Manage option parser
+    parser = ArgumentParser(
+        prog='Sorption',
+        description='Implements a sorption sink.'
+    )
+    parser.add_argument('filename')
+    parser.add_argument('-v', '--verbose', type=int, choices=range(0, 3),
+                        help="define verbose output level: 0=Min, 1=Medium, 2=Maximum.")
+    parser.add_argument('-t', '--test', action='store_true',
+                        help="do not run simulation, but output lists of available controls.")
+    parser.set_defaults(verbose=0, test=False)
+    args = parser.parse_args()
+
+    # ----- Process command line options -v/-t
+    verbose = args.verbose
+    test = args.test
+
+    prj_file_path = args.filename
+
+    if not os.path.exists(prj_file_path):
+        print(f"ERROR: PRJ file not found: {prj_file_path}")
+        return
+
+    msg_cmd = args
+    print(msg_cmd, "\n")
+
+    if verbose > 1:
+        print(f"cxLib attributes =>\n{chr(10).join(map(str, dir(cxLib)))}\n")
+
+    # ----- Initialize contamx-lib object w/ wp_mode and cb_option.
+    #   wp_mode = 0 => use wind pressure profiles, WTH and CTM files or
+    # associated API calls.
+    #   cb_option = True => set callback function to get PRJ INFO
+    # from the ContamXState.
+    my_prj = cxLib(prj_file_path, 0, True, callback_set_initial_values)
+    my_prj.setVerbosity(verbose)
+
+    # ----- Setup Simulation for PRJ
+    sim_not_ok = my_prj.setupSimulation(1)
+    if sim_not_ok > 0:
+        print(f"ABORT - sim_not_ok Returned by setupSimulation() = {sim_not_ok}")
+        print(" => invalid simulation parameters for co-simulation.")
+        my_prj.endSimulation()
+        return
+
+    err_count = check_controls(my_prj, test, PV_PASS_CTRL, CO_SET_CTRL)
+    if err_count > 0 or test is True:
+        return
+
+    # =========================================================================
+    # USER INPUT - Initialize controller class.
+    # =========================================================================
+
+    sorption_sink = Sorption(material)
+
+    # ----- Get simulation run info
+    start_day = my_prj.getSimStartDate()
+    end_day = my_prj.getSimEndDate()
+    start_time = my_prj.getSimStartTime()
+    end_time = my_prj.getSimEndTime()
+    dt = my_prj.getSimTimeStep()
+
+    sim_begin_sec = (start_day - 1) * 86400 + start_time
+    sim_end_sec = (end_day - 1) * 86400 + end_time
+
+    # ----- Calculate the simulation duration in seconds and total time steps
+    sim_begin_sec = (start_day - 1) * 86400 + start_time
+    sim_end_sec = (end_day - 1) * 86400 + end_time
+    if sim_begin_sec <= sim_end_sec:
+        sim_duration_sec = sim_end_sec - sim_begin_sec
+    else:
+        sim_duration_sec = 365 * 86400 - sim_end_sec + sim_begin_sec
+    num_time_steps = 0
+    if sim_duration_sec != 0:
+        num_time_steps = int(sim_duration_sec / dt)
+        print(f"PRJ settings => Transient simulation w/ {num_time_steps} time steps. (Sorption material: Am={material.Am}, Km={material.Km}, a={material.a}, b={material.b})")
+    else:
+        print("PRJ settings => Steady state simulation. (Sorption material: Am={material.Am}, Km={material.Km}, a={material.a}, b={material.b})")
+
+    # ----- Get the current date/time after initial steady state simulation
+    current_date = my_prj.getCurrentDayOfYear()
+    current_time = my_prj.getCurrentTimeInSec()
+    if verbose > 0:
+        print(f"Sim days = {start_day}:{end_day}")
+        print(f"Sim times = {start_time}:{end_time}")
+        print(f"Sim time step = {dt}")
+        print(f"Number of steps = {num_time_steps}")
+
+    # ----- Initialize result files
+
+    # ----- Output initial results.
+    current_date = my_prj.getCurrentDayOfYear()
+    current_time = my_prj.getCurrentTimeInSec()
+    Cr = my_prj.getOutputControlValue(PV_PASS_CTRL.index) # HCHO concn in room air [ug/m3]
+    print("day\ttime\tCr[ug/m3]\tS[ug/s]\tMs[ug/m2]\tCs[ug/m3]")
+    print(f"{current_date}\t{current_time}\t{Cr}\t0\t0\t0") # set initial values
+
+    # =========================================================================
+    # Run Simulation
+    # =========================================================================
+    for i in range(num_time_steps):
+
+        # =====================================================================
+        # Tasks to perform BEFORE current time step.
+        # =====================================================================
+        # Calculate sorption rate S, adsorbed mass Ms, and gas phase conc on material surface Cs, based on room air conc Cr
+
+        S = sorption_sink.get_S(Cr)
+        Ms = sorption_sink.get_Ms(dt)
+        Cs = sorption_sink.get_Cs()
+
+        my_prj.setInputControlValue(CO_SET_CTRL.index, -1 * S) # Set sorption sink to negative because it is defined with a generation rate in PRJ
+
+        # =====================================================================
+        # Run next time step.
+        # =====================================================================
+        my_prj.doSimStep(1)
+
+        # =====================================================================
+        # Tasks to perform AFTER current time step.
+        # =====================================================================
+        current_date = my_prj.getCurrentDayOfYear()
+        current_time = my_prj.getCurrentTimeInSec()
+        Cr = my_prj.getOutputControlValue(PV_PASS_CTRL.index)
+        print(f"{current_date}\t{current_time}\t{Cr}\t{S}\t{Ms}\t{Cs}")
+
+    my_prj.endSimulation()
+
+# --- End main() ---#
+
+
+if __name__ == "__main__":
+    main()
---
+++ material_adsorption.py
@@ -0,0 +1,104 @@
+"""
+material_adsorption.py - Surface adsorption model.
+
+Surface adsorption model developed based on literature
+Reference:
+1. Pétigny, N., J. Zhang, E. Horner, S. Steady, M. Chenal, G. Mialon, and V. Goletto. “Indoor Air Depolluting Material: Combining Sorption Testing and Modeling to Predict Product’s Service Life in Real Conditions.” Building and Environment 202 (September 2021): 107838. https://doi.org/10.1016/j.buildenv.2021.107838.
+
+"""
+from dataclasses import dataclass
+import math
+
+
+@dataclass
+class SorptionMaterial:
+    """
+    Am: Material surface [m2]
+    Km: Average room mass transfer coefficient
+    a: Sorption equilibrium constant a [-]
+    b: Sorption equilibrium constant b [-]
+    """
+    Am: float
+    Km: float
+    a: float
+    b: float
+
+# ====================================================== class PI_Control =====
+
+
+class Sorption:
+    """
+    Sorption equilibrium characteristics:
+        Ms(Cs) = CS * (a * Cs + b)
+    Mass balance in the sorption material:
+        Am * (dMs / dt) = S = Am * km * (Cr - Cs)
+    """
+    def __init__(self, sorption_material):
+        self.mat = sorption_material
+        self.Am = sorption_material.Am
+        self.Km = sorption_material.Km
+        self.a = sorption_material.a
+        self.b = sorption_material.b
+        self.Cs = 0.0   # Gas phase concentration on material surface [ug/m3]
+        self.Ms = 0.0   # Adsorbed mass concentration per unit surface area [ug/m2]
+        self.S = 0.0    # Adsorption rate
+
+    def get_S(self, Cr) -> float:
+        '''
+        Get the adsorption rate of the material, S [ug/s].
+
+        Args:
+            Cr: `float`
+                Current value of pollutant concentration in room air [ug/m3]
+
+        :returns: Adsorption rate [ug/s]
+        :rtype: float
+        '''
+        S = self.Am * self.Km * (Cr - self.Cs)
+        self.S = S
+        return S
+    
+    
+    def get_Ms(self, dt) -> float:
+        '''
+        Calculate adsorbed mass per unit area (Ms) of the material.
+
+        Args:
+            dt: `float`
+                Time step [s]
+
+        :returns: Adsorbed mass per unit area (Ms) [ug/m2]
+        :rtype: float
+
+        Governing equations:
+            S = Am * (dMs / dt) => dMs = S / Am * dt
+            Ms = Ms + dMs
+        '''
+        dMs = (self.S / self.Am) * dt   # mass adsorbed at time t (dt)
+        Ms = self.Ms + dMs
+        self.Ms = Ms         # set Ms of the material, ready for the next step
+        return Ms
+    
+
+    def get_Cs(self) -> float:
+        '''
+        Gas phase concentration on material surface (Cs).
+
+        Args:
+            
+
+        :returns: Gas phase concentration on surface, Cs [ug/m3]
+        :rtype: float
+
+        Sorption equilibrium characteristics:
+            Ms(Cs) = a * Cs * Cs + b * Cs = Cs * (a * Cs + b)
+        '''
+        discriminant = math.sqrt(self.b**2 - 4 * self.a * (-1 * self.Ms))
+        x1 = (-1 * self.b + discriminant) / (2 * self.a)
+        x2 = (-1 * self.b - discriminant) / (2 * self.a)
+
+        Cs = x1
+        if x1 > x2:
+            Cs = x2 # Cs is the smaller result
+        self.Cs = Cs
+        return Cs
